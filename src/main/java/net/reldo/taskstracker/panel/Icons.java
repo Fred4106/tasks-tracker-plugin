dede7ff34abf73cc11797d4440f94d10dd098a30
Nathaniel <natruf@gmail.com>
2026-02-01 07:24:17 -0600

    Refactor icon resource management for consistency and efficiency (#1)
    * ## Changes
    Refactor and optimize icon resource management across multiple components.
    
    - Moved icon resource declarations to a central `Icons` class for better reusability.
    - Eliminated duplicate icon definitions in `FilterButtonPanel` by consolidating them into `Icons`.
    - Updated `SearchBox` to use a shared `SEARCH_ICON` from `Icons` instead of a default library icon.
    - Introduced new icons (`MENU_EXPANDED_ICON`, `MENU_ICON_HOVER_SELECTED`) with improved resource handling.
    
    * patched to work with my plugin loader (not loading the same resource stream twice when restarting the plugin and creating the nav button.
    
    * Update RuneLite version to 1.12.14.1

--- src/main/java/net/reldo/taskstracker/panel/Icons.java
+++ src/main/java/net/reldo/taskstracker/panel/Icons.java
@@ -8,6 +8,7 @@ import net.runelite.client.util.ImageUtil;
 
 public class Icons
 {
+	public static BufferedImage PANEL_ICON = ImageUtil.loadImageResource(TasksTrackerPlugin.class, "panel_icon.png");
 	private static final String completeBtnPath = "panel/components/complete_button/";
 	public static final Icon INCOMPLETE_ONLY_ICON = new ImageIcon(ImageUtil.loadImageResource(TasksTrackerPlugin.class, completeBtnPath + "incomplete_only_icon.png"));
 	public static final Icon COMPLETE_ONLY_ICON = new ImageIcon(ImageUtil.loadImageResource(TasksTrackerPlugin.class, completeBtnPath + "complete_only_icon.png"));
@@ -29,13 +30,18 @@ public class Icons
 	public static final Icon DESCENDING_ICON = new ImageIcon(ImageUtil.loadImageResource(TasksTrackerPlugin.class, sortBtnPath + "descending_icon.png"));
 
 	private static final String expandBtnPath = "panel/components/";
-	public static final Icon MENU_EXPANDED_ICON = new ImageIcon(ImageUtil.loadImageResource(TasksTrackerPlugin.class, expandBtnPath + "filter_menu_expanded.png"));
 	public static final BufferedImage collapseImg = ImageUtil.loadImageResource(TasksTrackerPlugin.class, expandBtnPath + "filter_menu_collapsed.png");
 	public static final Icon MENU_ICON_HOVER = new ImageIcon(collapseImg);
+	public static final ImageIcon SEARCH_ICON = new ImageIcon(ImageUtil.loadImageResource(TasksTrackerPlugin.class, expandBtnPath + "search.png"));
 
 	public static final Icon MENU_COLLAPSED_ICON = new ImageIcon(ImageUtil.alphaOffset(collapseImg, -180));
 	public static final ImageIcon PLUS_ICON = new ImageIcon(ImageUtil.loadImageResource(TasksTrackerPlugin.class, "plus.png"));
 	public static final ImageIcon MINUS_ICON = new ImageIcon(ImageUtil.loadImageResource(TasksTrackerPlugin.class, "minus.png"));
 	public static final ImageIcon EYE_ICON = new ImageIcon(ImageUtil.loadImageResource(TasksTrackerPlugin.class, "eye.png"));
 	public static final ImageIcon EYE_CROSS_GREY = new ImageIcon(ImageUtil.loadImageResource(TasksTrackerPlugin.class, "eye-cross-grey.png"));
+
+	private static final BufferedImage expandedImg = ImageUtil.loadImageResource(TasksTrackerPlugin.class, expandBtnPath + "filter_buttons_expanded.png");
+	public static final Icon MENU_EXPANDED_ICON = new ImageIcon(ImageUtil.alphaOffset(expandedImg, -180));
+	public static final Icon MENU_ICON_HOVER_SELECTED = new ImageIcon(expandedImg);
+
 }
--- src/main/java/net/reldo/taskstracker/panel/filters/FilterButtonPanel.java
+++ src/main/java/net/reldo/taskstracker/panel/filters/FilterButtonPanel.java
@@ -9,7 +9,6 @@ import java.util.Map;
 import java.util.stream.Collectors;
 import javax.swing.Box;
 import javax.swing.BoxLayout;
-import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
@@ -26,6 +25,8 @@ import net.runelite.client.ui.FontManager;
 import net.runelite.client.util.ImageUtil;
 import net.runelite.client.util.SwingUtil;
 
+import static net.reldo.taskstracker.panel.Icons.*;
+
 @Slf4j
 public abstract class FilterButtonPanel extends FilterPanel
 {
@@ -37,14 +38,6 @@ public abstract class FilterButtonPanel extends FilterPanel
     protected JPanel buttonPanel;
 
     protected JToggleButton collapseBtn;
-    private final String expandBtnPath = "panel/components/";
-    private final BufferedImage collapseImg = ImageUtil.loadImageResource(TasksTrackerPlugin.class, expandBtnPath + "filter_buttons_collapsed.png");
-    private final Icon MENU_COLLAPSED_ICON = new ImageIcon(ImageUtil.alphaOffset(collapseImg, -180));
-    private final Icon MENU_ICON_HOVER = new ImageIcon(collapseImg);
-    private final BufferedImage expandedImg = ImageUtil.loadImageResource(TasksTrackerPlugin.class, expandBtnPath + "filter_buttons_expanded.png");
-    private final Icon MENU_EXPANDED_ICON = new ImageIcon(ImageUtil.alphaOffset(expandedImg, -180));
-    private final Icon MENU_ICON_HOVER_SELECTED = new ImageIcon(expandedImg);
-
 
     public FilterButtonPanel(TasksTrackerPlugin plugin, String label)
     {
@@ -75,9 +68,9 @@ public abstract class FilterButtonPanel extends FilterPanel
             button.setSelectedIcon(selectedIcon);
             button.setPreferredSize(new Dimension(image.getWidth(), image.getHeight() + 10));
         } else {
-	        button.setPreferredSize(new Dimension(button.getPreferredSize().width, 50));
+            button.setPreferredSize(new Dimension(button.getPreferredSize().width, 50));
         }
-        button.setToolTipText(tooltip.substring(0,1).toUpperCase() + tooltip.substring(1).toLowerCase());
+        button.setToolTipText(tooltip.substring(0, 1).toUpperCase() + tooltip.substring(1).toLowerCase());
 
         button.addActionListener(e -> {
             updateFilterText();
